7696b1c35554d009e91bce7532c18e34aa7e0356
arenasantiago <arenas4545a@gmail.com>
2024-12-06 02:12:42 -0500

    mandando

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ajusta las rutas si estás usando otras extensiones
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
